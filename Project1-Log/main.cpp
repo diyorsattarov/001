bc1e9cfc1079cdad0727b4fe06feb3c2e634f02d
diyor <diyorsattarov7@gmail.com>
2023-08-10 05:16:24 -0500

    [Issue/Problem] Boost logging to a file doesn't work as expected.
    [Description]
    Attempts were made to implement Boost logging to a file using the Boost.Log library, but the log files were not generated as intended. Despite trying different configurations and approaches, including initializing Boost.Log and using the appropriate settings, the log files were not created during program execution.
    
    [Solution]
    - Reviewed and revised the Boost.Log initialization and file logging code.
    - Ensured proper linking of the Boost.Log library.
    - Checked for compatibility between Boost.Log version, compiler, and system.
    - Verified the compilation and execution process for any errors or warnings.
    - Explored different path and permission configurations.
    - Investigated potential conflicting configurations or settings.
    
    [Outcome]
    The issue still persists, and despite several attempts, the log files are not being generated as expected. Further troubleshooting and consultation with experienced colleagues or online forums are suggested to resolve this problem.

--- Project1-Log/main.cpp
+++ Project1-Log/main.cpp
@@ -1,9 +1,15 @@
 #include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <iostream>
 #include <memory>
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+
+namespace logging = boost::log;
 
 
 // Interface Class to interact with LogSystem and MockLogSystem
@@ -23,6 +29,11 @@ public:
 class LogSystem : public ILogSystem 
 {
 public:
+    LogSystem()
+    {
+        logging::core::get()->set_filter(logging::trivial::severity >= logging::trivial::trace);
+    }
+
     void trace(const std::string& message) override 
     {
         BOOST_LOG_TRIVIAL(trace) << message;
@@ -59,6 +70,20 @@ using MyLibPtr = std::shared_ptr < ILogSystem >;
 class LogSystemMock : public ILogSystem
 {
 public:
+    LogSystemMock()
+    {
+        std::cout << "test123" << std::endl;
+        logging::add_file_log(
+            logging::keywords::file_name = "my_log.log",
+            logging::keywords::rotation_size = 10 * 1024 * 1024,
+            logging::keywords::auto_flush = true,
+            logging::keywords::format = (
+                logging::expressions::stream << logging::trivial::severity << ": "
+                << logging::expressions::smessage
+                )
+        );
+    }
+
     virtual ~LogSystemMock() {}
     MOCK_METHOD(void, trace, (const std::string& message), (override));
     MOCK_METHOD(void, debug, (const std::string& message), (override));
@@ -73,7 +98,19 @@ class Myapp
 protected:
     MyLibPtr m_mylib;
 public:
-    Myapp(MyLibPtr mylib) : m_mylib(mylib) {}
+    Myapp(MyLibPtr mylib) : m_mylib(mylib) 
+    {
+        std::cout << "test123" << std::endl;
+        logging::add_file_log(
+            logging::keywords::file_name = "my_log1.log",
+            logging::keywords::rotation_size = 10 * 1024 * 1024,
+            logging::keywords::auto_flush = true,
+            logging::keywords::format = (
+                logging::expressions::stream << logging::trivial::severity << ": "
+                << logging::expressions::smessage
+                )
+        );
+    }
     void triggerTrace() 
     {
         m_mylib->trace("This is a trace message");
@@ -105,7 +142,8 @@ public:
     }
 };
 
-TEST(LoggingTest, TriggerLoggingLevels) {
+TEST(LoggingTest, TriggerLoggingLevels)
+{
     auto mockLog = std::make_shared<LogSystemMock>();
     Myapp app(mockLog);
 
