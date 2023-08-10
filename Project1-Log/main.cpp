2bcce692a05bdc09a67b4fdb22fa107e0c8f0b56
diyor <diyorsattarov7@gmail.com>
2023-08-10 04:40:35 -0500

    feat: Add test cases for logging levels in Myapp class
    Implemented test cases in the LoggingTest suite to validate the behavior of triggering different logging levels using the Myapp class. The test cases utilize a LogSystemMock to set expectations on the mock log methods for each logging level. This ensures that the Myapp class correctly invokes the appropriate logging methods based on the triggered actions.
    
    The LoggingTest suite includes individual tests for each logging level, covering trace, debug, info, warning, error, and fatal levels. Expectations are set for each level to ensure proper interaction between Myapp and the mock log system.

--- Project1-Log/main.cpp
+++ Project1-Log/main.cpp
@@ -8,7 +8,8 @@
 
 // Interface Class to interact with LogSystem and MockLogSystem
 
-class ILogSystem {
+class ILogSystem 
+{
 public:
     virtual ~ILogSystem() {}
     virtual void trace(const std::string& message) = 0;
@@ -19,15 +20,39 @@ public:
     virtual void fatal(const std::string& message) = 0;
 };
 
-/*
-class LogSystem
+class LogSystem : public ILogSystem 
 {
 public:
-    virtual ~ILogSystem() {}
-    ....
-    virtual void fatal() override {}
+    void trace(const std::string& message) override 
+    {
+        BOOST_LOG_TRIVIAL(trace) << message;
+    }
+
+    void debug(const std::string& message) override 
+    {
+        BOOST_LOG_TRIVIAL(debug) << message;
+    }
+
+    void info(const std::string& message) override 
+    {
+        BOOST_LOG_TRIVIAL(info) << message;
+    }
+
+    void warning(const std::string& message) override 
+    {
+        BOOST_LOG_TRIVIAL(warning) << message;
+    }
+
+    void error(const std::string& message) override 
+    {
+        BOOST_LOG_TRIVIAL(error) << message;
+    }
+
+    void fatal(const std::string& message) override 
+    {
+        BOOST_LOG_TRIVIAL(fatal) << message;
+    }
 };
-*/
 
 using MyLibPtr = std::shared_ptr < ILogSystem >;
 
@@ -45,20 +70,67 @@ public:
 
 class Myapp
 {
+protected:
     MyLibPtr m_mylib;
 public:
     Myapp(MyLibPtr mylib) : m_mylib(mylib) {}
+    void triggerTrace() 
+    {
+        m_mylib->trace("This is a trace message");
+    }
+
+    void triggerDebug() 
+    {
+        m_mylib->debug("This is a debug message");
+    }
+
+    void triggerInfo() 
+    {
+        m_mylib->info("This is an info message");
+    }
+
+    void triggerWarning() 
+    {
+        m_mylib->warning("This is a warning message");
+    }
+
+    void triggerError() 
+    {
+        m_mylib->error("This is an error message");
+    }
+
+    void triggerFatal() 
+    {
+        m_mylib->fatal("This is a fatal message");
+    }
 };
 
-TEST(LoggingTest, TraceMessage) {
-    LogSystemMock mockLog;
-    EXPECT_CALL(mockLog, trace("This is a trace message")).Times(1);
+TEST(LoggingTest, TriggerLoggingLevels) {
+    auto mockLog = std::make_shared<LogSystemMock>();
+    Myapp app(mockLog);
 
-    // Call the method that uses the logging functionality
-    // and expects it to call the mock method trace("This is a trace message")
+    // Set expectations for each logging level
+    EXPECT_CALL(*mockLog, trace("This is a trace message")).Times(1);
+    EXPECT_CALL(*mockLog, debug("This is a debug message")).Times(1);
+    EXPECT_CALL(*mockLog, info("This is an info message")).Times(1);
+    EXPECT_CALL(*mockLog, warning("This is a warning message")).Times(1);
+    EXPECT_CALL(*mockLog, error("This is an error message")).Times(1);
+    EXPECT_CALL(*mockLog, fatal("This is a fatal message")).Times(1);
+
+    app.triggerTrace();
+    app.triggerDebug();
+    app.triggerInfo();
+    app.triggerWarning();
+    app.triggerError();
+    app.triggerFatal();
+
+    // Verify the expected method calls on the mock
+    ::testing::Mock::VerifyAndClearExpectations(mockLog.get());
 }
 
-int main(int argc, char** argv) {
-    ::testing::InitGoogleMock(&argc, argv);
-    return RUN_ALL_TESTS();
+
+int main(int argc, char** argv) 
+{
+	::testing::InitGoogleMock(&argc, argv);
+	return RUN_ALL_TESTS();
 }
